86987b52aba5eadd10f1aa05c031ecd86add6d5b
Joe K <jkrilov@gmail.com>
2023-04-03 14:46:05 -0400

    add: link

--- nicegui/app.py
+++ nicegui/app.py
@@ -3,6 +3,8 @@ from nicegui import ui
 
 ui.label('NiceGUI Running on Azure Container Apps')
 ui.label('Deployed via GitHub Actions')
-
+with ui.row():
+	ui.icon('link').classes('text-2xl')
+	ui.link('Joey40/NiceGuiTest', 'https://github.com/Joey40/NiceGuiTest')
 
 ui.run()
