8fb65d12a6a9ca7bd508a6fccaddc02f432539a6
Joe K <jkrilov@gmail.com>
2023-04-03 14:23:01 -0400

    change label name

--- nicegui/app.py
+++ nicegui/app.py
@@ -1,7 +1,7 @@
 """Test NiceGUI in Azure Web App."""
 from nicegui import ui
 
-ui.label('NiceGUI Running on Azure Web App')
+ui.label('NiceGUI Running on Azure Container Apps')
 ui.label('Deployed via GitHub Actions')
 
 
