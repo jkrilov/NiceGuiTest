892a0cdad5c26c4841053acbd0b456b40a96f046
Joe K <jkrilov@gmail.com>
2023-04-03 10:04:32 -0400

    feat: switch to ACA

---
+++ terraform/aca.tf
@@ -0,0 +1,24 @@
+
+resource "azurerm_log_analytics_workspace" "nicegui" {
+  name                = "${var.projectName}-LAW"
+  location            = azurerm_resource_group.baseRG.location
+  resource_group_name = azurerm_resource_group.baseRG.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_registry" "nicegui" {
+  name                = "${var.projectName}-ACR"
+  resource_group_name = azurerm_resource_group.baseRG.name
+  location            = azurerm_resource_group.baseRG.location
+  sku                 = "Basic"
+  admin_enabled       = true
+
+}
+
+resource "azurerm_container_app_environment" "nicegui" {
+  name                       = "${var.projectName}-ACE"
+  location                   = azurerm_resource_group.baseRG.location
+  resource_group_name        = azurerm_resource_group.baseRG.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.nicegui.id
+}
---
+++ terraform/keyvault.tf
@@ -0,0 +1,42 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "nicegui" {
+  name                        = "${var.projectName}-KV"
+  location                    = azurerm_resource_group.baseRG.location
+  resource_group_name         = azurerm_resource_group.baseRG.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Get",
+      "List",
+      "Set"
+    ]
+
+  }
+}
+
+resource "azurerm_key_vault_secret" "ACR-URL" {
+  name         = "ACR-URL"
+  value        = azurerm_container_registry.nicegui.login_server
+  key_vault_id = azurerm_key_vault.nicegui.id
+}
+
+resource "azurerm_key_vault_secret" "ACR-USER" {
+  name         = "ACR-URL"
+  value        = azurerm_container_registry.nicegui.admin_username
+  key_vault_id = azurerm_key_vault.nicegui.id
+}
+
+resource "azurerm_key_vault_secret" "ACR-PASS" {
+  name         = "ACR-URL"
+  value        = azurerm_container_registry.nicegui.admin_password
+  key_vault_id = azurerm_key_vault.nicegui.id
+}
