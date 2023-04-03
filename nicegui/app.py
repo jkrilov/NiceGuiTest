1d6064453af200bee5f930542afd73dcc3a58723
Joe K <jkrilov@gmail.com>
2023-04-03 08:48:40 -0400

    feat: port

--- nicegui/app.py
+++ nicegui/app.py
@@ -4,6 +4,5 @@ from nicegui import ui
 ui.label('NiceGUI Running on Azure Web App')
 ui.label('Deployed via GitHub Actions')
 
-ui.run(
-  port='8000' #default port used by Azure Web Apps
-)
+
+ui.run()
