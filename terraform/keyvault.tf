5a56628d2369cf1001ebf73af4d1acfbf7dc58ca
Joe K <jkrilov@gmail.com>
2023-04-03 10:15:27 -0400

    fix: whitespace

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -1,12 +1,12 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "nicegui" {
-  name                        = "${var.projectName}-KV"
-  location                    = azurerm_resource_group.baseRG.location
-  resource_group_name         = azurerm_resource_group.baseRG.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+  name                       = "${var.projectName}-KV"
+  location                   = azurerm_resource_group.baseRG.location
+  resource_group_name        = azurerm_resource_group.baseRG.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
 
   sku_name = "standard"
 
