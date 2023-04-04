77767168b7c9ec51afc92d120094a468085cf26b
Joe K <jkrilov@gmail.com>
2023-04-04 10:47:55 -0400

    add: images

--- nicegui/app.py
+++ nicegui/app.py
@@ -5,10 +5,10 @@ app.add_static_files('/images', 'images')
 ui.label('NiceGUI example running on Azure Container Apps')
 ui.label('Deployed via GitHub Actions')
 with ui.row():
-	#ui.avatar('img:./images/github.png', size='md', font_size='32px', color='White')
+	ui.avatar('img:./images/github.png', size='md', font_size='32px', color='White')
 	ui.link('Joey40/NiceGuiTest', 'https://github.com/Joey40/NiceGuiTest')
 
 ui.run(
-    title='NiceGUI ACA Test'
-    #favicon='./images/favicon.ico'
+    title='NiceGUI ACA Test',
+    favicon='./images/favicon.ico'
 )
