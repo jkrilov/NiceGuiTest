cd93f64fbdf14a998b2d5d2aa6f29b1336a43331
Joe K <jkrilov@gmail.com>
2023-04-05 13:33:37 -0400

    add: easter egg route

--- nicegui/app.py
+++ nicegui/app.py
@@ -2,15 +2,22 @@
 from nicegui import app, ui
 
 app.add_static_files('/images', 'images')
-ui.label('NiceGUI example running on Azure Container Apps')
-ui.label('Deployed via GitHub Actions')
-with ui.row():
-	ui.avatar('img:./images/github.png', size='md', font_size='24px', color='White')
-	ui.link('Joey40/NiceGuiTest', 'https://github.com/Joey40/NiceGuiTest')
-with ui.row():
-    with ui.button('Click me!', on_click=lambda: badge.set_text(int(badge.text) + 1)):
-        badge = ui.badge('0', color='red').props('floating')
-    ui.button('Reset', on_click=lambda: badge.set_text('0'))
+
+@ui.page('/')
+def home_page():
+    ui.label('NiceGUI example running on Azure Container Apps')
+    ui.label('Deployed via GitHub Actions')
+    with ui.row():
+        ui.avatar('img:./images/github.png', size='md', font_size='24px', color='White')
+        ui.link('Joey40/NiceGuiTest', 'https://github.com/Joey40/NiceGuiTest')
+    with ui.row():
+        with ui.button('Click me!', on_click=lambda: badge.set_text(int(badge.text) + 1)):
+            badge = ui.badge('0', color='red').props('floating')
+        ui.button('Reset', on_click=lambda: badge.set_text('0'))
+
+@ui.page('/egg')
+def egg():
+    ui.label('You found an Easter Egg!')
 
 ui.run(
     title='NiceGUI ACA Test',
