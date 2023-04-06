9ed10f06e8e92e040d3031df592799f77e0cf28f
Joe K <jkrilov@gmail.com>
2023-04-06 08:22:41 -0400

    feat: adjust font size

--- nicegui/theme.py
+++ nicegui/theme.py
@@ -9,7 +9,7 @@ def frame(navtitle: str):
     '''Custom page frame to share the same styling and behavior across all pages'''
     ui.colors(primary='#6E93D6', secondary='#53B689', accent='#111B1E', positive='#53B689')
     with ui.header().classes('justify-between text-white'):
-        ui.label('NiceGUI ACA Example').classes('font-bold')
+        ui.label('NiceGUI ACA Example').classes('font-bold, text-3xl')
         ui.label(navtitle)
         with ui.row():
             menu()
